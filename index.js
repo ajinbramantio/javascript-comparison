67042615fb6861cf211c2586432070cf9fe71308
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-31 11:18:07 +0700

    Improve code quality

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const gradeE = 25
 
 function gradeLevel(score) {
   let result = ''
+
   if (typeof score == 'number') {
+    // Determine result grade if the score is a number
     if (score <= gradeA && score > gradeB) {
-      result = 'GRADE  A'
+      result = 'GRADE A'
     } else if (score <= gradeB && score > gradeC) {
       result = 'GRADE B'
     } else if (score <= gradeC && score > gradeD) {
@@ -18,11 +20,25 @@ function gradeLevel(score) {
     } else if (score < gradeE && score >= 0) {
       result = 'GRADE E'
     } else if (score < 0 || score > gradeA) {
-      result = 'score Not Valid'
+      result = 'Score Not Valid'
     }
-    return result
   } else {
-    return (result = 'not number')
+    // Will not determine the result grade
+    // because the score is not a number
+    result = 'not number'
   }
+
+  return result
 }
-console.log(gradeLevel(0))
+
+const gradeResult1 = gradeLevel(0)
+const gradeResult2 = gradeLevel(99)
+const gradeResult3 = gradeLevel(100)
+const gradeResult4 = gradeLevel(40)
+const gradeResult5 = gradeLevel(20)
+
+console.log(gradeResult1)
+console.log(gradeResult2)
+console.log(gradeResult3)
+console.log(gradeResult4)
+console.log(gradeResult5)
